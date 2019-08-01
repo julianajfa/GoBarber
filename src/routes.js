5ea7f6dcff9085de2bdff85966668eb59cb859e6
fernandes <julianafernandes@id.uff.br>
2019-08-01 17:48:09 -0300

    Fixing and adding

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: ' Hello Juliana' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Juliana Fernandes',
+    email: 'julianafernandes@id.uff.br',
+    password_hash: '1234456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
